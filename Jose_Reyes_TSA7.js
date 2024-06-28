2d29d6c0386b779f254e38057fc20d304a08a6be
leleyyyyyyyy <167260839+leleyyyyyyyy@users.noreply.github.com>
2024-06-28 08:42:52 +0800

    Update Jose_Reyes_TSA7.js

--- Jose_Reyes_TSA7.js
+++ Jose_Reyes_TSA7.js
@@ -1,73 +1,91 @@
-function setGap(){
-    let gapValue = document.getElementById("spacing").value;
-    let flexContainer = document.getElementById("flex-wrapper");
-    flexContainer.style.gap = ${gapValue}px;
-    console.log(Gap: ${flexContainer.style.gap});
+function resetFlexbox() {
+    const flexWrapper = document.getElementById('flex-wrapper');
+    flexWrapper.style.flexDirection = 'row';
+    flexWrapper.style.justifyContent = 'flex-start';
+    flexWrapper.style.alignItems = 'flex-start';
+    flexWrapper.style.gap = '10px';
+
+    for (let i = 1; i <= 3; i++) {
+        const item = document.getElementById(item${i});
+        item.style.flexGrow = '0';
+    }
+
+    document.getElementById('spacing').value = 0;
+    document.getElementById('grow1').value = 0;
+    document.getElementById('grow2').value = 0;
+    document.getElementById('grow3').value = 0;
+}
+
+function setGap() {
+    const spacing = document.getElementById('spacing').value;
+    const flexWrapper = document.getElementById('flex-wrapper');
+    flexWrapper.style.gap = ${spacing}px;
+}
+
+function setDirection(option) {
+    const flexWrapper = document.getElementById('flex-wrapper');
+    if (option === 1) {
+        flexWrapper.style.flexDirection = 'row';
+    } else if (option === 2) {
+        flexWrapper.style.flexDirection = 'column';
+    }
 }
 
 function setJustifyContent(option) {
-    let flexContainer = document.getElementById("flex-wrapper");
-    if (option === 1) {
-        flexContainer.style.justifyContent = "start";
-    } else if (option === 2){
-        flexContainer.style.justifyContent = "center";
-    } else if (option === 3){
-        flexContainer.style.justifyContent = "end";
-    } else if (option === 4){
-        flexContainer.style.justifyContent = "space-between";
-    } else if (option === 5){
-        flexContainer.style.justifyContent = "space-around";
-    } else if (option === 6){
-        flexContainer.style.justifyContent = "space-evenly";
+    const flexWrapper = document.getElementById('flex-wrapper');
+    switch (option) {
+        case 1:
+            flexWrapper.style.justifyContent = 'flex-start';
+            break;
+        case 2:
+            flexWrapper.style.justifyContent = 'center';
+            break;
+        case 3:
+            flexWrapper.style.justifyContent = 'flex-end';
+            break;
+        case 4:
+            flexWrapper.style.justifyContent = 'space-between';
+            break;
+        case 5:
+            flexWrapper.style.justifyContent = 'space-around';
+            break;
+        case 6:
+            flexWrapper.style.justifyContent = 'space-evenly';
+            break;
     }
-    console.log(Justify-Content: ${flexContainer.style.justifyContent});
 }
 
 function setAlignItems(option) {
-    let flexContainer = document.getElementById("flex-wrapper");
-    if (option === 1){
-        flexContainer.style.alignItems = "start";
-    } else if (option === 2){
-        flexContainer.style.alignItems = "center";
-    } else if (option === 3){
-        flexContainer.style.alignItems = "end";
-    } else if (option === 4){
-        flexContainer.style.alignItems = "stretch";
+    const flexWrapper = document.getElementById('flex-wrapper');
+    switch (option) {
+        case 1:
+            flexWrapper.style.alignItems = 'flex-start';
+            break;
+        case 2:
+            flexWrapper.style.alignItems = 'center';
+            break;
+        case 3:
+            flexWrapper.style.alignItems = 'flex-end';
+            break;
     }
-    console.log(Align-Items: ${flexContainer.style.alignItems});
 }
 
-function setDirection(option){
-    let flexContainer = document.getElementById("flex-wrapper");
-    if (option === 1){
-        flexContainer.style.flexDirection = "row";
-    } else if (option === 2){
-        flexContainer.style.flexDirection = "column";
-    }
-    console.log(Flex-Direction: ${flexContainer.style.flexDirection});
-}
-
-function setGrow(option){
-    let growValue1 = document.getElementById("grow1").value;
-    let growValue2 = document.getElementById("grow2").value;
-    let growValue3 = document.getElementById("grow3").value;
-    let itemBox1 = document.getElementById("item1");
-    let itemBox2 = document.getElementById("item2");
-    let itemBox3 = document.getElementById("item3");
-    if (option === 1) {
-        itemBox1.style.flexGrow = growValue1;
-    } else if (option === 2) {
-        itemBox2.style.flexGrow = growValue2;
-    } else if (option === 3) {
-        itemBox3.style.flexGrow = growValue3;
-    } else if (option === 0) {
-        itemBox1.style.flexGrow = '0';
-        itemBox2.style.flexGrow = '0';
-        itemBox3.style.flexGrow = '0';
+function setGrow(option) {
+    if (option === 0) {
+        for (let i = 1; i <= 3; i++) {
+            const item = document.getElementById(item${i});
+            item.style.flexGrow = '0';
+            document.getElementById(grow${i}).value = 0; // Reset grow value to 0
+        }
     } else if (option === 4) {
-        itemBox1.style.flexGrow = growValue1;
-        itemBox2.style.flexGrow = growValue2;
-        itemBox3.style.flexGrow = growValue3;
+        for (let i = 1; i <= 3; i++) {
+            const growValue = document.getElementById(grow${i}).value;
+            const item = document.getElementById(item${i});
+            item.style.flexGrow = growValue;
+        }
+    } else {
+        const growValue = document.getElementById(grow${option}).value;
+        const item = document.getElementById(item${option});
+        item.style.flexGrow = growValue;
     }
-    console
 }
